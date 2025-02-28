49406fbe8742169a90348c572faa357dca559c36
ujangbedog <domba88galing@gmail.com>
2025-02-28 22:03:27 +0700

    add: not found

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="notfound">
+      <h1>404 Not Found</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .notfound {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
