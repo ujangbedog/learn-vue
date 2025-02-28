4f25a9a334cc0c489822a2f5e86d7129ccad27e8
ujangbedog <domba88galing@gmail.com>
2025-02-28 22:03:45 +0700

    fix: error

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        { src: "src/static/*", dest: "" }, // Copy semua file di src/static ke dist/
+      ],
+    }),
+    // vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
