2220acaedb4d7b466e9478bce090adafe9ebcc2f
ujangbedog <domba88galing@gmail.com>
2025-03-03 23:21:42 +0700

    using mode hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import TugasSatu from "../views/tugas/TugasSatu.vue";
 import NotFoundView from "../views/NotFoundView.vue";
@@ -6,7 +6,7 @@ import TugasDua from "@/views/tugas/TugasDua.vue";
 import TugasTiga from "@/views/tugas/TugasTiga.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
