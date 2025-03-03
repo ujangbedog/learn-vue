6c131c9741ca331be5356210891f88859d86f81f
ujangbedog <domba88galing@gmail.com>
2025-03-03 21:16:18 +0700

    layouting

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar fixed top-0 left-0 w-full bg-green-500 shadow-md p-4 flex justify-center gap-5">
+      <RouterLink to="/" class="nav-link text-white font-bold transition duration-300 hover:text-gray-900">Home</RouterLink>
+      <RouterLink to="/about" class="nav-link text-white font-bold transition duration-300 hover:text-gray-900">About</RouterLink>
+    </nav>
+  </template>
+  
+  <script setup>
+  import { RouterLink } from 'vue-router';
+  </script>
+  
+  <style scoped>
+  </style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
       ],
     }),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
