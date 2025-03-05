666a17783a41d66b95e690f15391308e065f5b0c
ujangbedog <domba88galing@gmail.com>
2025-03-05 22:49:29 +0700

    mod: 404 not found page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,16 +1,43 @@
 <template>
-    <div class="notfound">
-      <h1>404 Not Found</h1>
-    </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .notfound {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
+  <v-container fill-height>
+    <v-row align="center" justify="center" style="height: 70vh;">
+      <v-col cols="12" md="8" class="text-center">
+        <h1 class="text-h1 font-weight-bold">404</h1>
+        <h4 class="text-h4 font-weight-bold">
+          <span v-for="(char, index) in text" :key="index" v-text="char" class="typing"></span>
+        </h4>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fullText: "Not Found",
+      text: "",
+      index: 0
+    };
+  },
+  mounted() {
+    this.typeText();
+  },
+  methods: {
+    typeText() {
+      if (this.index < this.fullText.length) {
+        this.text += this.fullText[this.index];
+        this.index++;
+        setTimeout(this.typeText, 100);
+      }
     }
   }
-  </style>
-  
+};
+</script>
+
+<style>
+.typing {
+  display: inline-block;
+  white-space: pre;
+}
+</style>
