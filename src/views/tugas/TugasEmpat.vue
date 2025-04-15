aef165257ae5e5ea94608e573f0cea1070baadc3
ujangbedog <domba88galing@gmail.com>
2025-04-15 08:27:50 +0700

    feat and mod: tugas

---
+++ src/views/tugas/TugasEmpat.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-container fluid>
+      <v-row>
+        <!-- Sidebar untuk Tugas 4 -->
+        <Sidebar 
+          title="Praktikum Tugas 4" 
+          :praktikumNumber="4"
+          :praktikumList="[
+            { 
+              name: 'Template - Data Raw', 
+              route: '/tugas/3/template-data-raw' 
+            },
+            { 
+              name: 'Template - Data Attribute', 
+              route: '/tugas/3/template-data-attribute' 
+            },
+            { 
+              name: 'Properti Template', 
+              route: '/tugas/3/properti-template' 
+            },
+            { 
+              name: 'Properti Methods', 
+              route: '/tugas/3/properti-methods' 
+            },
+            { 
+              name: 'Properti Computed', 
+              route: '/tugas/3/properti-computed' 
+            },
+            { 
+              name: 'Properti Filters', 
+              route: '/tugas/3/properti-filters' 
+            },
+            { 
+              name: 'Latihan - Template', 
+              route: '/tugas/3/latihan-template' 
+            },
+            { 
+              name: 'Latihan - Kalkulator', 
+              route: '/tugas/3/latihan-kalkulator' 
+            },
+          ]"
+        />
+  
+        <!-- Konten Utama -->
+        <router-view></router-view> <!-- Tempat menampilkan konten praktikum -->
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script setup>
+  import Sidebar from "@/components/Sidebar.vue";
+  </script>
+  
+  <style scoped>
+  .content {
+    background-color: #010100;
+    min-height: 100vh;
+    color: white;
+  }
+  </style>
