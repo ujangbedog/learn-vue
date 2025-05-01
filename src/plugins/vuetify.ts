aff6dece56f29c4f303b01cfe0f04dd4c0cbc9c5
ujangbedog <domba88galing@gmail.com>
2025-05-01 23:13:45 +0700

    chore: use dark mode

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,3 +1,4 @@
+// plugins/vuetify.js
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
@@ -6,4 +7,29 @@ import * as directives from "vuetify/directives";
 export default createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: "dark",
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          background: "#FFFFFF",
+          surface: "#FFFFFF",
+          primary: "#6200EE",
+          secondary: "#03DAC6",
+          error: "#B00020",
+        },
+      },
+      dark: {
+        dark: true,
+        colors: {
+          background: "#121212",
+          surface: "#1E1E1E",
+          primary: "#BB86FC",
+          secondary: "#03DAC6",
+          error: "#CF6679",
+        },
+      },
+    },
+  },
 });
