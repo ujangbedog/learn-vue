485f078579542c6bc4f281b6641a0a107b778048
ujangbedog <domba88galing@gmail.com>
2025-05-01 14:20:37 +0700

    fix: trailing slash

--- src/views/tugas/6/simple-todo.vue
+++ src/views/tugas/6/simple-todo.vue
@@ -56,7 +56,7 @@
   const newDescription = ref("");
   const todos = ref([]);
   
-  const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL.replace(/\/+$/, '');
   
   const fetchTodos = async () => {
     try {
