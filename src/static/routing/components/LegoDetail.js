da5e7d1604ccae045225ce488df5039f237c5d16
ujangbedog <domba88galing@gmail.com>
2025-05-17 10:51:59 +0700

    feat: routing finallized

--- src/static/routing/components/LegoDetail.js
+++ src/static/routing/components/LegoDetail.js
@@ -1,25 +1,39 @@
 import { products } from '../data.js'
 
 export default {
+    props: {
+        id: {
+            type: String,
+            required: true
+        }
+    },
     data() {
         return {
             lego: null
         }
     },
     created() {
-        const legoId = parseInt(this.$route.params.id)
+        const legoId = parseInt(this.id)
         this.lego = products.find(p => p.id === legoId)
+    },
+    beforeRouteLeave(to, from, next) {
+        const confirmed = window.confirm('Apakah Anda yakin ingin meninggalkan halaman LEGO?');
+        if (confirmed) {
+            next();
+        } else {
+            next(false);
+        }
     },
     template: `
         <div v-if="lego" class="lego-detail">
             <h2>{{ lego.name }}</h2>
             <img :src="lego.image" :alt="lego.name">
             <p class="price">Rp {{ lego.price.toLocaleString() }}</p>
-            <router-link to="/legos" class="back-link">← Back to LEGO Collection</router-link>
+            <router-link to="/legos" class="back-link">Kembali ke daftar LEGO</router-link>
         </div>
         <div v-else>
-            <h2>LEGO not found</h2>
-            <router-link to="/legos">Back to LEGO Collection</router-link>
+            <h2>LEGO tidak ditemukan</h2>
+            <router-link to="/legos">Kembali ke daftar LEGO</router-link>
         </div>
     `
-} 
+}
