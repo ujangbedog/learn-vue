54022ef86633937406e5f62f801263815f5b0a29
ujangbedog <domba88galing@gmail.com>
2025-05-17 10:33:14 +0700

    refactor: assets to main after static

--- src/static/UTS/assets/Home.js
+++ src/static/UTS/assets/Home.js
@@ -12,7 +12,7 @@ export default {
               <button class="btn btn-warning btn-lg" @click="$emit('navigate', 'collection')">Lihat Koleksi</button>
             </div>
             <div class="col-lg-6">
-              <img src="./assets/images/HERO.jpg" alt="Lego Hero Image" class="img-fluid rounded">
+              <img src="../assets/images/HERO.jpg" alt="Lego Hero Image" class="img-fluid rounded">
             </div>
           </div>
         </div>
@@ -26,7 +26,7 @@ export default {
             <div class="col-md-4 mb-4">
               <div class="card h-100 text-center p-4">
                 <div class="card-body">
-                  <img src="./assets/images/original.png" alt="Original" class="mb-3" width="80">
+                  <img src="../assets/images/original.png" alt="Original" class="mb-3" width="80">
                   <h5>Produk Original</h5>
                   <p>Semua produk Lego kami dijamin 100% original dan resmi.</p>
                 </div>
@@ -35,7 +35,7 @@ export default {
             <div class="col-md-4 mb-4">
               <div class="card h-100 text-center p-4">
                 <div class="card-body">
-                  <img src="./assets/images/delivery.png" alt="Delivery" class="mb-3" width="80">
+                  <img src="../assets/images/delivery.png" alt="Delivery" class="mb-3" width="80">
                   <h5>Pengiriman Cepat</h5>
                   <p>Nikmati pengiriman cepat ke seluruh Indonesia dalam 2-3 hari kerja.</p>
                 </div>
@@ -44,7 +44,7 @@ export default {
             <div class="col-md-4 mb-4">
               <div class="card h-100 text-center p-4">
                 <div class="card-body">
-                  <img src="./assets/images/customer-service.png" alt="Service" class="mb-3" width="80">
+                  <img src="../assets/images/customer-service.png" alt="Service" class="mb-3" width="80">
                   <h5>Layanan Pelanggan</h5>
                   <p>Tim layanan pelanggan kami siap membantu 24/7.</p>
                 </div>
@@ -61,7 +61,7 @@ export default {
           <div class="row">
             <div class="col-md-4 mb-4">
               <div class="card h-100">
-                <img src="./assets/images/LEGO-Star-Wars-X-Wing-Starfighter.png" alt="New Arrival 1" class="card-img-top product-image">
+                <img src="../assets/images/LEGO-Star-Wars-X-Wing-Starfighter.png" alt="New Arrival 1" class="card-img-top product-image">
                 <div class="card-body">
                   <h5 class="card-title">LEGO Star Wars X-Wing Starfighter™</h5>
                   <p class="card-text">Rp 4.599.000</p>
@@ -71,7 +71,7 @@ export default {
             </div>
             <div class="col-md-4 mb-4">
               <div class="card h-100">
-                <img src="./assets/images/LEGO-City-Central-Train-Station.png" alt="New Arrival 2" class="card-img-top product-image">
+                <img src="../assets/images/LEGO-City-Central-Train-Station.png" alt="New Arrival 2" class="card-img-top product-image">
                 <div class="card-body">
                   <h5 class="card-title">LEGO City Train Station</h5>
                   <p class="card-text">Rp 1.699.000</p>
@@ -81,7 +81,7 @@ export default {
             </div>
             <div class="col-md-4 mb-4">
               <div class="card h-100">
-                <img src="./assets/images/LEGO-Technic-McLaren-P1.png" alt="New Arrival 3" class="card-img-top product-image">
+                <img src="../assets/images/LEGO-Technic-McLaren-P1.png" alt="New Arrival 3" class="card-img-top product-image">
                 <div class="card-body">
                   <h5 class="card-title">LEGO Technic McLaren P1™</h5>
                   <p class="card-text">Rp 8.699.000</p>
